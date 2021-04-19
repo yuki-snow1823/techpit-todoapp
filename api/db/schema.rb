56e0c1ece1f47d66817b2d46571565266f1d1a01
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-04-20 07:39:15 +0900

    add: ユーザー関連のデータの作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_231920) do
+ActiveRecord::Schema.define(version: 2021_04_19_223521) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
