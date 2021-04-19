f072e6bfa3e14b514aa9e92677ac1f30bcfa1acb
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-04-20 07:34:16 +0900

    until: 2-4

---
+++ api/app/controllers/v1/todos_controller.rb
@@ -0,0 +1,29 @@
+class V1::TodosController < ApplicationController
+  # 表示テスト用のアクション
+  def index
+    todo = Todo.all
+    render json: todo
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo
+    else
+      render json: todo.errors
+    end
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    if todo.destroy
+      render json: todo
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :user_id)
+  end
+end
