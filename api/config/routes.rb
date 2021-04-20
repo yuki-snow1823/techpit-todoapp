04a9c9090092a9ba5ceabbbd8a0532c7d6fe4921
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-04-20 11:42:49 +0900

    add: userルーティングの追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :todos, only: [:index, :create, :destroy]
+    resources :users, only: [:index, :create]
   end
 end
