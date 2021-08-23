199ab65e4d6ee39af67bfa95c3520fd6be64e6df
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-08-23 13:59:36 +0900

    addtodoコンポーネントの作成

---
+++ front/components/AddTodo.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field v-model="title" counter="10" label="todo" required></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-btn @click="handleSubmit">作成</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
