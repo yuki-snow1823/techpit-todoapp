a4e7d143e38a32ea8bba5696c7a652159232499f
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-10-12 06:03:12 +0900

    add: mypage

---
+++ front/pages/mypage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-if="user">
+      <p>Email: {{ user.email }}</p>
+      <p>名前: {{ user.name }}</p>
+    </div>
+    <v-btn @click="logOut">LOGOUT</v-btn>
+  </div>
+</template>
+
+<script>
+  import firebase from "@/plugins/firebase";
+  export default {
+    computed: {
+      user() {
+        return this.$store.state.auth.currentUser;
+      },
+    },
+    methods: {
+      async logOut() {
+        await firebase
+          .auth()
+          .signOut()
+          .catch((error) => {
+            console.log(error);
+          });
+
+        this.$store.dispatch("auth/setUser", null);
+        this.$router.push("/login");
+      },
+    },
+  };
+
+</script>
+
+<style>
+</style>
