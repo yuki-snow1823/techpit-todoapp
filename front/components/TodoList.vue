832b97835d76579ae31f70a6c9ed24635091f678
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-10-23 10:20:07 +0900

    add: todoリストの追加

--- front/components/TodoList.vue
+++ front/components/TodoList.vue
@@ -5,30 +5,65 @@
       <v-spacer></v-spacer>
       <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
     </v-card-title>
-    <v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
+    <v-data-table :headers="headers" :items="todos" :search="search">
+      <!-- ここから追加 -->
+      <template v-slot:item.action="{ item }">
+        <v-icon small @click="deleteItem(item)">delete</v-icon>
+      </template>
+      <!-- ここまで追加 -->
+    </v-data-table>
   </v-card>
 </template>
 
 <script>
-export default {
-  props: ["todos"],
-  data() {
-    return {
-      search: "",
-      headers: [
-        {
-          text: "タイトル",
-          align: "left",
-          value: "title"
-        },
-        { 
-          text: "ユーザー名", 
-          value: "username" 
+  import axios from "@/plugins/axios";
+  
+  export default {
+    props: ["todos"],
+    data() {
+      return {
+        search: "",
+        headers: [{
+            text: "タイトル",
+            align: "left",
+            value: "title"
+          },
+          {
+            text: "ユーザー名",
+            value: "username"
+          },
+          // ここから追加
+          {
+            text: "Actions",
+            value: "action",
+          },
+          // ここまで追加
+        ]
+      };
+    },
+    computed: {
+      user() {
+        return this.$store.state.auth.currentUser;
+      }
+    },
+    methods: {
+      async deleteItem(item) {
+        const res = confirm("本当に削除しますか？");
+        if (res) {
+          await axios.delete(`/v1/todos/${item.id}`);
+          const todos = this.user.todos.filter((todo) => {
+            return todo.id !== item.id;
+          });
+          const newUser = {
+            ...this.user,
+            todos,
+          };
+          this.$store.dispatch("auth/setUser", newUser);
         }
-      ]
-    };
-  }
-};
+      },
+    }
+  };
+
 </script>
 
 <style>
