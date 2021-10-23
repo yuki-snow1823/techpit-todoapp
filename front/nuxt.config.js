446a450cb637d49b410ab3574f68922cb6e4beb0
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-10-23 10:19:12 +0900

    add: iconの設定

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -32,7 +32,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "@/plugins/authCheck" // ここを追記
+    "@/plugins/authCheck",
+    "@/plugins/vuetify"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
